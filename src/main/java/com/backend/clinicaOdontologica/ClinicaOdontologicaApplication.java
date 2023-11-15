cd17fc208b36bebebe1c937759a5f685c963cad4
juan triventi <jctriventi@ikeasistencia.com.ar>
2023-11-14 23:10:14 -0300

    agrego create.sql manualmente en main

--- src/main/java/com/backend/clinicaOdontologica/ClinicaOdontologicaApplication.java
+++ src/main/java/com/backend/clinicaOdontologica/ClinicaOdontologicaApplication.java
@@ -1,34 +1,34 @@
 package com.backend.clinicaOdontologica;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class ClinicaOdontologicaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaOdontologicaApplication.class, args);
-		crearTabla();
+		System.out.println("Aplicacion corriendo en el puerto 8080");
 	}
 
-	private static void crearTabla(){
-		Connection connection = null;
-		try {
-			Class.forName("org.h2.Driver");
-			connection = DriverManager.getConnection("jdbc:h2:~/test;INIT=RUNSCRIPT FROM 'create.sql'", "sa", "");
-
-		} catch (Exception e){
-			e.printStackTrace();
-		} finally {
+	@Bean
+	public CommandLineRunner run(JdbcTemplate jdbcTemplate) {
+		return args -> {
 			try {
-				connection.close();
-			} catch (Exception ex){
-				ex.printStackTrace();
+				jdbcTemplate.execute("DROP TABLE IF EXISTS DOMICILIOS");
+				jdbcTemplate.execute("CREATE TABLE DOMICILIOS (ID INT AUTO_INCREMENT PRIMARY KEY, CALLE VARCHAR(50) NOT NULL, NUMERO INT NOT NULL, LOCALIDAD VARCHAR(50) NOT NULL, PROVINCIA VARCHAR(50) NOT NULL)");
+
+				jdbcTemplate.execute("DROP TABLE IF EXISTS PACIENTES");
+				jdbcTemplate.execute("CREATE TABLE PACIENTES (ID INT AUTO_INCREMENT PRIMARY KEY, NOMBRE VARCHAR(100) NOT NULL, APELLIDO VARCHAR(100) NOT NULL, DNI INT NOT NULL, FECHA DATE NOT NULL, DOMICILIO_ID INT)");
+
+				jdbcTemplate.execute("DROP TABLE IF EXISTS ODONTOLOGOS");
+				jdbcTemplate.execute("CREATE TABLE ODONTOLOGOS (ID INT AUTO_INCREMENT PRIMARY KEY, MATRICULA VARCHAR(100) NOT NULL, NOMBRE VARCHAR(100) NOT NULL, APELLIDO VARCHAR(100) NOT NULL)");
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		}
+		};
 	}
 }
