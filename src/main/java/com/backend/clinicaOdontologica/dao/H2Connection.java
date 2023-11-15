30c11ca41741ec96f9be67acb10a1baf85e77ec6
juan triventi <jctriventi@ikeasistencia.com.ar>
2023-11-14 22:42:49 -0300

    cambio nombre de la base en H2Connection

--- src/main/java/com/backend/clinicaOdontologica/dao/H2Connection.java
+++ src/main/java/com/backend/clinicaOdontologica/dao/H2Connection.java
@@ -7,6 +7,6 @@ import java.sql.SQLException;
 public class H2Connection {
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("org.h2.Driver");
-        return DriverManager.getConnection("jdbc:h2:~/clinica", "sa", "");
+        return DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
     }
 }
